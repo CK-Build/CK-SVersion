e163674c0a4b4720e4b5f4325265f67220e0a8e9
Olivier Spinelli <olivier.spinelli@signature-code.com>
2026-06-12 10:40:18 +0200

    Cleanup using.

--- CK.SVersion/SVersion.cs
+++ CK.SVersion/SVersion.cs
@@ -2,8 +2,6 @@ using System;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
-using System.IO;
-using System.Numerics;
 using System.Text.RegularExpressions;
 
 namespace CK.Core;
--- CK.SVersion/SVersionBound.cs
+++ CK.SVersion/SVersionBound.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Diagnostics;
 using System.Text;
-using System.Threading;
-using System.Xml.Resolvers;
 
 namespace CK.Core;
 
